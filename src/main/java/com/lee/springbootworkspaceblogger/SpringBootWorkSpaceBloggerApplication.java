33607da84f80c6a0ea8078f1c12701b6f97c604e
s0911602565 <s0911602565@gmail.com>
2024-06-21 23:45:18 +0800

    add memo

--- src/main/java/com/lee/springbootworkspaceblogger/SpringBootWorkSpaceBloggerApplication.java
+++ src/main/java/com/lee/springbootworkspaceblogger/SpringBootWorkSpaceBloggerApplication.java
@@ -8,7 +8,16 @@ import org.springframework.boot.web.servlet.ServletComponentScan;
 public class SpringBootWorkSpaceBloggerApplication {
 
     public static void main(String[] args) {
+        //SQL 8.4.0
+        //mysql -u root -p12345678 mysql
+        //create database board;
+        //mysql -u root -p12345678 < D:\board.sql
+        // http://127.0.0.1:8080/blog/index
+        // 點選登入
+        //帳:lee 密碼:1
+
         SpringApplication.run(SpringBootWorkSpaceBloggerApplication.class, args);
+
     }
 
 }
